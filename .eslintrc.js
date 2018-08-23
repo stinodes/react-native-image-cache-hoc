a969968b351d144a3a2c24a7176f9cf3b4c410e7
Stijn Tytgat <stinodesigns@gmail.com>
2018-08-23 10:13:00 +0200

    Added code for pruning specific files

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
     'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'no-var': 'error',
